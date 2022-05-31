8036bca307464dde1cd65aaae200742023ce9367
mintymilkx <auliaanz95@gmaill.com>
2022-05-31 18:03:19 +0700

    Menambahkan code untuk menampilkan pop-up

--- app/src/main/java/com/example/project/capstone/eden/ChooseGachaActivity.kt
+++ app/src/main/java/com/example/project/capstone/eden/ChooseGachaActivity.kt
@@ -2,10 +2,33 @@ package com.example.project.capstone.eden
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import com.example.project.capstone.eden.databinding.ActivityChooseGachaBinding
 
 class ChooseGachaActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityChooseGachaBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_choose_gacha)
+        binding = ActivityChooseGachaBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        showGachaResult()
+
+    }
+
+    fun showGachaResult(){
+        binding.btnExchange.setOnClickListener {
+            val view = View.inflate(this, R.layout.popup_gacha, null)
+
+            val builder = AlertDialog.Builder(this)
+            builder.setView(view)
+
+            val dialog = builder.create()
+            dialog.window?.setBackgroundDrawableResource(android.R.color.transparent)
+
+        }
+
+
     }
 }
