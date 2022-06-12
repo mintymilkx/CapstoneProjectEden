02e2d3d37838e9570fd333b75a7b9216fb1985e6
mintymilkx <auliaanz95@gmaill.com>
2022-06-12 09:50:09 +0700

    Add code to show list donations

---
+++ app/src/main/java/com/example/project/capstone/eden/ui/donasi/DonasiModel.kt
@@ -0,0 +1,9 @@
+package com.example.project.capstone.eden.ui.donasi
+
+data class DonasiModel(
+    val amount: String? = null,
+    val photoUrl: String? = null,
+    val organizationName: String? = null,
+    val title: String? = null,
+    val description: String? = null,
+)
