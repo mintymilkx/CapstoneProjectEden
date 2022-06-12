c4cdf08b06481e06c1e15ac38a5a1c8a2d4a2dab
mintymilkx <auliaanz95@gmaill.com>
2022-06-12 09:50:51 +0700

    Move activity

--- app/src/main/java/com/example/project/capstone/eden/activity/HomeActivity.kt
+++ app/src/main/java/com/example/project/capstone/eden/activity/HomeActivity.kt
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.example.project.capstone.eden.R
-import com.example.project.capstone.eden.fragment.DonasiFragment
+import com.example.project.capstone.eden.ui.donasi.DonasiFragment
 import com.example.project.capstone.eden.fragment.GachaFragment
 import com.example.project.capstone.eden.fragment.HomeFragment
 import com.example.project.capstone.eden.fragment.MyPlantsFragment
@@ -17,7 +17,7 @@ class HomeActivity : AppCompatActivity() {
     @Suppress("DEPRECATION")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_on_boarding)
+        setContentView(R.layout.activity_home)
 
         val homeFragment = HomeFragment()
         val myPlantsFragment = MyPlantsFragment()
