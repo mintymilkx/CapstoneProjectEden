54b864bccfbe2cd92eafa9c98fc6362b0cae784b
pranaditakusuma <komangpranadita@gmail.com>
2022-06-12 10:11:25 +0700

    initial activity (coba-coba)

--- app/src/main/java/com/example/project/capstone/eden/activity/MyPlantsDetailActivity.kt
+++ app/src/main/java/com/example/project/capstone/eden/activity/MyPlantsDetailActivity.kt
@@ -3,10 +3,17 @@ package com.example.project.capstone.eden.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.project.capstone.eden.R
+import com.example.project.capstone.eden.databinding.ActivityMyPlantsDetailBinding
 
 class MyPlantsDetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMyPlantsDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_my_plants_detail)
+
+        binding = ActivityMyPlantsDetailBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
     }
 }
