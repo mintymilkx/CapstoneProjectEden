b191bb642fac6284d7f37f8b2bdb6ada7b031053
pranaditakusuma <komangpranadita@gmail.com>
2022-06-13 12:43:26 +0700

    bismillah push

--- app/src/main/java/com/example/project/capstone/eden/ui/scan/PlantDetailAfterScanningActivity.kt
+++ app/src/main/java/com/example/project/capstone/eden/ui/scan/PlantDetailAfterScanningActivity.kt
@@ -14,7 +14,7 @@ import com.example.project.capstone.eden.ui.myplant.MyPlantsFragment
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
-import com.squareup.picasso.Picasso
+//import com.squareup.picasso.Picasso
 
 class PlantDetailAfterScanningActivity : AppCompatActivity() {
 
@@ -39,105 +39,105 @@ class PlantDetailAfterScanningActivity : AppCompatActivity() {
         val benefit = binding.tvBenefitDescriptionAfterScan
         val imgPlant = binding.ivPlant
 
-        if (resultPlant != null) {
-            txt_names = resultPlant
-
-        } else{
-            if (model_name != null) {
-                txt_names = model_name
-            }
-        }
-
-        val docRef = db.collection("plants").document("${txt_names}")
-
-        docRef.get()
-            .addOnSuccessListener{ document ->
-                if(document != null) {
-
-                    Log.d("exist", "DocumentSnapshot data: ${document.data}")
-
-                    plantName.text = document.getString("name")
-
-                } else {
-                    Log.d("sorry there is no exist", "No Such Document")
-                }
-            }
-            .addOnFailureListener {exception ->
-                Log.d("sorry you get an error", "get failed with", exception)
-            }
-
-        val docRef2 = db.collection("plants").document("${txt_names}")
-
-        docRef2.get()
-            .addOnSuccessListener{ document ->
-                if(document != null) {
-
-                    Log.d("exist", "DocumentSnapshot data: ${document.data}")
-
-                    type.text = document.getString("type")
-
-                } else {
-                    Log.d("sorry there is no exist", "No Such Document")
-                }
-            }
-            .addOnFailureListener {exception ->
-                Log.d("sorry you get an error", "get failed with", exception)
-            }
-
-        val docRef3 = db.collection("plants").document("${txt_names}")
-
-        docRef3.get()
-            .addOnSuccessListener{ document ->
-                if(document != null) {
-
-                    Log.d("exist", "DocumentSnapshot data: ${document.data}")
-
-                    pyshicalApp.text = document.getString("pyshicalApp")
-
-                } else {
-                    Log.d("sorry there is no exist", "No Such Document")
-                }
-            }
-            .addOnFailureListener {exception ->
-                Log.d("sorry you get an error", "get failed with", exception)
-            }
-
-        val docRef4 = db.collection("plants").document("${txt_names}")
-
-        docRef4.get()
-            .addOnSuccessListener{ document ->
-                if(document != null) {
-
-                    Log.d("exist", "DocumentSnapshot data: ${document.data}")
-
-                    benefit.text = document.getString("benefit")
-
-                } else {
-                    Log.d("sorry there is no exist", "No Such Document")
-                }
-            }
-            .addOnFailureListener {exception ->
-                Log.d("sorry you get an error", "get failed with", exception)
-            }
-
-        val docRef5 = db.collection("plants").document("${txt_names}")
-
-        docRef5.get()
-            .addOnSuccessListener{ document ->
-                if(document != null) {
-
-                    Log.d("exist", "DocumentSnapshot data: ${document.data}")
-
-                    var link = document.getString("imageUrl")
-                    Picasso.get().load(link).into(imgPlant)
-
-                } else {
-                    Log.d("sorry there is no exist", "No Such Document")
-                }
-            }
-            .addOnFailureListener {exception ->
-                Log.d("sorry you get an error", "get failed with", exception)
-            }
+//        if (resultPlant != null) {
+//            txt_names = resultPlant
+//
+//        } else{
+//            if (model_name != null) {
+//                txt_names = model_name
+//            }
+//        }
+//
+//        val docRef = db.collection("plants").document("${txt_names}")
+//
+//        docRef.get()
+//            .addOnSuccessListener{ document ->
+//                if(document != null) {
+//
+//                    Log.d("exist", "DocumentSnapshot data: ${document.data}")
+//
+//                    plantName.text = document.getString("name")
+//
+//                } else {
+//                    Log.d("sorry there is no exist", "No Such Document")
+//                }
+//            }
+//            .addOnFailureListener {exception ->
+//                Log.d("sorry you get an error", "get failed with", exception)
+//            }
+//
+//        val docRef2 = db.collection("plants").document("${txt_names}")
+//
+//        docRef2.get()
+//            .addOnSuccessListener{ document ->
+//                if(document != null) {
+//
+//                    Log.d("exist", "DocumentSnapshot data: ${document.data}")
+//
+//                    type.text = document.getString("type")
+//
+//                } else {
+//                    Log.d("sorry there is no exist", "No Such Document")
+//                }
+//            }
+//            .addOnFailureListener {exception ->
+//                Log.d("sorry you get an error", "get failed with", exception)
+//            }
+//
+//        val docRef3 = db.collection("plants").document("${txt_names}")
+//
+//        docRef3.get()
+//            .addOnSuccessListener{ document ->
+//                if(document != null) {
+//
+//                    Log.d("exist", "DocumentSnapshot data: ${document.data}")
+//
+//                    pyshicalApp.text = document.getString("pyshicalApp")
+//
+//                } else {
+//                    Log.d("sorry there is no exist", "No Such Document")
+//                }
+//            }
+//            .addOnFailureListener {exception ->
+//                Log.d("sorry you get an error", "get failed with", exception)
+//            }
+//
+//        val docRef4 = db.collection("plants").document("${txt_names}")
+//
+//        docRef4.get()
+//            .addOnSuccessListener{ document ->
+//                if(document != null) {
+//
+//                    Log.d("exist", "DocumentSnapshot data: ${document.data}")
+//
+//                    benefit.text = document.getString("benefit")
+//
+//                } else {
+//                    Log.d("sorry there is no exist", "No Such Document")
+//                }
+//            }
+//            .addOnFailureListener {exception ->
+//                Log.d("sorry you get an error", "get failed with", exception)
+//            }
+//
+//        val docRef5 = db.collection("plants").document("${txt_names}")
+//
+//        docRef5.get()
+//            .addOnSuccessListener{ document ->
+//                if(document != null) {
+//
+//                    Log.d("exist", "DocumentSnapshot data: ${document.data}")
+//
+////                    var link = document.getString("imageUrl")
+////                    Picasso.get().load(link).into(imgPlant)
+//
+//                } else {
+//                    Log.d("sorry there is no exist", "No Such Document")
+//                }
+//            }
+//            .addOnFailureListener {exception ->
+//                Log.d("sorry you get an error", "get failed with", exception)
+//            }
 
 
         binding.btCloseInAfterScan.setOnClickListener{
